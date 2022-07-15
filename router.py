c14a79300315f772c49510425e6113b1487f1bb8
francischi <francischiooo@gmail.com>
2022-07-15 23:03:59 +0800

    7/15 adjust router

--- router.py
+++ router.py
@@ -2,8 +2,6 @@ from flask import Blueprint
 from Controllers.UserController import UserController
 allRoute = Blueprint('allRoute', __name__)
 
-# allRoute.route('/user/create', methods=['POST'])(UserController().createUser)
-# allRoute.route('/user/getAll', methods=['GET'])(UserController().getAll)
 @allRoute.route('/user/create', methods=['POST'])
 def createUser():
     return UserController().createUser()
