f68834cc1738e893e21a10f3f75337c812d946ad
francischi <francischiooo@gmail.com>
2022-07-15 22:41:25 +0800

    7/15 adjust router

--- router.py
+++ router.py
@@ -2,5 +2,12 @@ from flask import Blueprint
 from Controllers.UserController import UserController
 allRoute = Blueprint('allRoute', __name__)
 
-allRoute.route('/user/create', methods=['POST'])(UserController().createUser)
-allRoute.route('/user/getAll', methods=['GET'])(UserController().getAll)
+# allRoute.route('/user/create', methods=['POST'])(UserController().createUser)
+# allRoute.route('/user/getAll', methods=['GET'])(UserController().getAll)
+@allRoute.route('/user/create', methods=['POST'])
+def createUser():
+    return UserController().createUser()
+
+@allRoute.route('/user/getAll', methods=['GET'])
+def getAll():
+    return UserController().getAll()
